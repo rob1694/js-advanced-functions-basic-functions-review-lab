a160e47c793ae2d4bbd1514f4e450d55ecc61996
rob1694 <robertb1694@gmail.com>
2021-04-06 22:18:46 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+function saturdayFun(todo = "roller-skate") {
+    return `This Saturday, I want to ${todo}!`
+  }
+
+function mondayWork(todo = "go to the office") {
+    return `This Monday, I will ${todo}.`
+}
+
+function wrapAdjective(star = "*") {
+    return function(word = 'special') {
+        return `You are ${star}${word}${star}!`;
+    } 
+}
+
+const Calculator = {
+  add: function(a,b) {
+    return a + b;
+  },
+  subtract: function(a,b) {
+    return a - b
+  },
+  multiply: function(a,b) {
+    return a * b
+  },
+  divide: function(a,b) {
+    return a / b;
+  }
+}
+
+function actionApplyer(start, array) {
+  let inte = start
+
+  for (let i = 0; i < array.length; i++ ){
+    inte = array[i](inte)
+  }
+
+  return inte
+}
